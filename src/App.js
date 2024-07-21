760f7d0bb048ebb652bcef1105e05eda88c38899
Kemal Kujovic <kemalkujovic1111@gmail.com>
2024-07-21 17:53:08 +0200

    Install && setup react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import ErrorPage from "./pages/ErrorPage";
+import ProjectsPage from "./pages/ProjectsPage";
 
 function App() {
-  return (
-      <Home />
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <ErrorPage />,
+    },
+    { path: "/project:projectsId", element: <ProjectsPage /> },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
