9ef462a76ea24619066e5a9b09cf059ca4e8d9cc
Kemal Kujovic <kemalkujovic1111@gmail.com>
2024-08-10 23:46:48 +0200

    Implement contact form validation

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { ContactWrapper, Email } from "./ContactElements";
 import { MdContentCopy } from "react-icons/md";
 import { IconButton, Tooltip } from "@mui/material";
@@ -8,6 +8,43 @@ import ScrollAnimation from "react-animate-on-scroll";
 
 function Contact() {
   const [showTooltip, setShowTooltip] = useState(false);
+  const [errors, setErrors] = useState({});
+  const [inputValue, setInputValue] = useState({
+    user_name: "",
+    user_email: "",
+    message: "",
+  });
+
+  const nameValidation = (value) => {
+    return value.trim().length < 3
+      ? "Ime mora imati najmanje 3 karaktera."
+      : "";
+  };
+
+  const emailValidation = (value) => {
+    return value.trim().length < 3
+      ? "Ime mora imati najmanje 3 karaktera."
+      : "";
+  };
+
+  const messageValidation = (value) => {
+    return value.trim().length < 10
+      ? "Ime mora imati najmanje 3 karaktera."
+      : "";
+  };
+
+  const validation = {
+    user_name: nameValidation,
+    user_email: emailValidation,
+    message: messageValidation,
+  };
+
+  const refs = {
+    user_name: useRef(null),
+    user_email: useRef(null),
+    message: useRef(null),
+  };
+
   const copyToClipboard = () => {
     navigator.clipboard.writeText("kemalkujovic99@gmail.com");
     setShowTooltip(true);
@@ -16,18 +53,56 @@ function Contact() {
     }, 700);
   };
 
-  const onSubmit = async () => {
-    const data = {
-      name: "Kemal",
-      email: "kemalkujovic1111@gmail.com",
-      message: "Hello from node.js server!",
-    };
-    try {
-      const res = await axios.post("http://localhost:3000/send", data);
-      console.log(res.data);
-    } catch (error) {
-      console.log(error);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newErrors = {};
+    let fristErorr = null;
+
+    Object.keys(inputValue).forEach((field) => {
+      const error = validation[field](inputValue[field]);
+      if (error) {
+        newErrors[field] = error;
+        if (!fristErorr) {
+          fristErorr = field;
+        }
+      }
+    });
+
+    setErrors(newErrors);
+
+    if (fristErorr) {
+      refs[fristErorr].current.focus();
     }
+
+    const formIsValid = Object.values(errors).every((error) => !error);
+
+    const data = {
+      name: inputValue.user_name,
+      email: inputValue.user_email,
+      message: inputValue.message,
+    };
+
+    if (formIsValid) {
+      try {
+        const res = await axios.post("http://localhost:3000/send", data);
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setInputValue((prevValue) => ({
+      ...prevValue,
+      [name]: value,
+    }));
+
+    const error = validation[name](value);
+    setErrors({ ...errors, [name]: error });
   };
 
   return (
@@ -36,7 +111,57 @@ function Contact() {
         <div className="SectionTitle">Get In Touch</div>
         <ScrollAnimation animateIn="fadeIn">
           <div className="BigCard">
-            <Email>
+            <form onSubmit={handleSubmit}>
+              <div className="card-contact">
+                <label htmlFor="user_name">Name*</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="user_name"
+                  required
+                  onChange={(e) => handleChange(e)}
+                  ref={refs.user_name}
+                  value={inputValue.user_name}
+                />
+                {errors.user_name && (
+                  <p style={{ color: "red" }}>{errors.user_name}</p>
+                )}
+              </div>
+              <div className="card-contact">
+                <label htmlFor="user_email">E-mail*</label>
+                <input
+                  type="text"
+                  id="email"
+                  name="user_email"
+                  required
+                  onChange={(e) => handleChange(e)}
+                  ref={refs.user_email}
+                  value={inputValue.user_email}
+                />
+                {errors.user_email && (
+                  <p style={{ color: "red" }}>{errors.user_email}</p>
+                )}
+              </div>
+              <div className="card-contact">
+                <label htmlFor="message">Message*</label>
+                <input
+                  type="text"
+                  id="message"
+                  name="message"
+                  required
+                  onChange={(e) => handleChange(e)}
+                  value={inputValue.message}
+                  ref={refs.message}
+                />
+                {errors.message && (
+                  <p style={{ color: "red" }}>{errors.message}</p>
+                )}
+              </div>
+              <div className="button-wrap">
+                <button>SEND</button>
+              </div>
+            </form>
+            {/* <Email>
               <div
                 style={{
                   display: "flex",
@@ -77,8 +202,7 @@ function Contact() {
               >
                 Send Email
               </a>
-            </Email>
-            <button onClick={onSubmit}>SEND MAIL</button>
+            </Email> */}
           </div>
         </ScrollAnimation>
       </div>
